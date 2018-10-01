d491f8fb4c7610e80453c6dba1d6c96edb2d90f4
robear22890 <robgilmore127@gmail.com>
2018-10-01 18:51:40 -0500

    Added vlookup function to the utils.R file.

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,27 @@
+#' @title Mock Excel "VlookUp" Function
+#' @description A function that mimicks excels vlookup, but for R's dataframe.
+#' @param lookup_data A vector of items to look up.
+#' @param df The dataframe to search.
+#' @param match_data The column name to search in the dataframe.
+#' @param return_data The column of data to return when matched.
+#' @return A vector that contains the items of interest.
+#' @pretty_print TRUE
+#' @details A function that works like the VLOOKUP function in excel.  This function was
+#' borrowed from \url{https://www.r-bloggers.com/an-r-vlookup-not-so-silly-idea/}.
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @export
+#' @family Data Manipulators
+#' @rdname vlookup
+vlookup <- function(lookup_data, df, match_data, return_data) {
+  # TODO: Update the way this returns data.  Allow it to add data to a new column.
+  m <- match(lookup_data, df[[match_data]])
+  df[[return_data]][m]
+}
 
 
 set_path <- function() {
@@ -7,7 +31,6 @@ set_path <- function() {
   # The next line set the working directory to the relevant one:
   setwd(dirname(current_path))
 }
-
 
 
 
