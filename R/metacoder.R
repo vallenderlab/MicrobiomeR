e946750bf97928d8a025cd5db683c5ea2e8890ac
grabear <robgilmore127@gmail.com>
2018-12-10 17:35:49 -0600

    Added the taxon_id_filter and taxa_prevalence_filter function.

--- R/metacoder.R
+++ R/metacoder.R
@@ -90,6 +90,7 @@ metacoder_comp_func_1 <- function(abund_1, abund_2) {
 #' @return Returns a metacoder object with samples that pass the filters.
 #' @details Get the samples to keep by using purr and the user supplied transform and filter + condition formulas.
 #' The purr packge allows the use of anonymous functions as described in the link below:
+#'
 #' \url{https://jennybc.github.io/purrr-tutorial/ls03_map-function-syntax.html#anonymous_function,_formula}
 #' @examples
 #' \dontrun{
@@ -117,7 +118,7 @@ sample_filter <- function(obj, .f_transform = NULL, .f_filter = NULL, .f_conditi
   if (!is.null(.f_filter) && !is.null(.f_condition)) {
 
     # Validate and get otu_abundance data
-    mo_clone <- validate_MicrobiomeR_format(metacoder_object = mo_clone, valid_formats = c("raw_format", "basic_format"),
+    mo_clone <- validate_MicrobiomeR_format(obj = mo_clone, valid_formats = c("raw_format", "basic_format"),
                                             force_format = TRUE, validated = validated, min_or_max = min)
     abund_data <- taxa::get_dataset(obj = mo_clone, data = "otu_abundance")
 
@@ -128,8 +129,6 @@ sample_filter <- function(obj, .f_transform = NULL, .f_filter = NULL, .f_conditi
       sample_cols <- transformer(.data = abund_data, func = .f_transform) %>% dplyr::select_if(is.numeric)
     }
     # Get the samples to keep by using purr and the user supplied filter and condition formulas
-    # The purr packge allows the use of anonymous functions as described in the link below:
-    # https://jennybc.github.io/purrr-tutorial/ls03_map-function-syntax.html#anonymous_function,_formula
     samples_to_keep <- purrr::map(sample_cols, .f_filter, list(...)) %>% # Apply a summary function like 'sum' or 'mean'
       purrr::map(.f_condition) %>% # Apply a function to the sample data that does some comparison (f(x)x>10000)
       purrr::keep(~ . == TRUE) %>%
@@ -139,6 +138,69 @@ sample_filter <- function(obj, .f_transform = NULL, .f_filter = NULL, .f_conditi
     other_vars <- abund_data %>% dplyr::select_if(function(x) is.numeric(x) == FALSE) %>% colnames() #%>% purrr::discard(~.=="taxon_id")
     mo_clone$data$otu_abundance  <- dplyr::select(abund_data, c(other_vars, samples_to_keep))
     mo_clone$data$sample_data <- mo_clone$data$sample_data %>% dplyr::filter(sample_id %in% samples_to_keep)
+    return(mo_clone)
+  } else {
+    stop("You have to supply a filter formula AND a condition formula.")
+  }
+}
+
+
+#' @title Filter Taxon Ids from Metacoder Objects
+#' @description This function provides a flexible way to filter unwanted taxon_ids from the taxmap object and from the
+#' observations of a MicrobiomeR formatted object.
+#' @inheritParams sample_filter
+#' @return Returns a metacoder object with taxon_ids that pass the filters.
+#' @pretty_print TRUE
+#' @details Get the taxon_ids to keep by using purr and the user supplied transform and filter + condition formulas.
+#' The purr packge allows the use of anonymous functions as described in the link below:
+#'
+#' \url{https://jennybc.github.io/purrr-tutorial/ls03_map-function-syntax.html#anonymous_function,_formula}
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @export
+#' @family Metacoder Filters
+#' @rdname taxon_id_filter
+#' @seealso
+#'  \code{\link[MicrobiomeR]{validate_MicrobiomeR_format}},  \code{\link[MicrobiomeR]{transposer}},  \code{\link[MicrobiomeR]{transformer}}
+#'
+#'  \code{\link[taxa]{get_dataset}},  \code{\link[taxa]{filter_taxa}}
+#'
+#'  \code{\link[dplyr]{select_all}}
+#'
+#'  \code{\link[purrr]{map}},  \code{\link[purrr]{keep}}
+#' @importFrom taxa get_dataset filter_taxa
+#' @importFrom dplyr select_if
+#' @importFrom purrr map keep
+taxon_id_filter <- function(obj, .f_transform = NULL, .f_filter = NULL, .f_condition = NULL, validated = FALSE, ...) {
+  mo_clone <- obj$clone()
+  # Make sure the required functions are provided.
+  if (!is.null(.f_filter) && !is.null(.f_condition)) {
+
+    # Validate and get taxa_abundance data
+    mo_clone <- validate_MicrobiomeR_format(obj = mo_clone, valid_formats = c("raw_format", "basic_format"),
+                                            force_format = TRUE, validated = validated, min_or_max = min)
+    abund_data <- taxa::get_dataset(obj = mo_clone, data = "taxa_abundance")
+    trans_data <- transposer(.data = abund_data, ids = "taxon_id", header_name = "samples")
+
+    #  Get the taxon_id data columns to work with
+    if (is.null(.f_transform)) { # Get raw sample data
+      taxon_id_cols <- trans_data %>% dplyr::select_if(is.numeric)
+    } else { # Get transfmormed sample data
+      taxon_id_cols <- transformer(.data = trans_data, func = .f_transform) %>% dplyr::select_if(is.numeric)
+    }
+    # Get the samples to keep by using purr and the user supplied filter and condition formulas
+    # The purr packge allows the use of anonymous functions as described in the link below:
+    # https://jennybc.github.io/purrr-tutorial/ls03_map-function-syntax.html#anonymous_function,_formula
+    taxon_ids_to_keep <- purrr::map(taxon_id_cols, .f_filter, list(...)) %>% # Apply a summary function like 'sum' or 'mean'
+      purrr::map(.f_condition) %>% # Apply a function to the sample data that does some comparison (f(x)x>10000)
+      purrr::keep(~ . == TRUE) %>%
+      names() # Determine which samples to keep
+    # Update the observation data tables and the taxmap object
+    mo_clone <- taxa::filter_taxa(mo_clone, taxon_ids %in% taxon_ids_to_keep, reassign_obs = FALSE)
     return(mo_clone)
   } else {
     stop("You have to supply a filter formula AND a condition formula.")
@@ -156,7 +218,8 @@ sample_filter <- function(obj, .f_transform = NULL, .f_filter = NULL, .f_conditi
 #' @param validated This parameter provides a way to override validation steps.  Use carefully.  Default: FALSE
 #' @return A taxmap object that has been agglomerated at the specified rank.
 #' @pretty_print TRUE
-#' @details This function helps analyzing taxonomic data at intermediate ranks.
+#' @details This function helps analyzing taxonomic data at intermediate ranks by agglomerating the observation data
+#' and the taxmap object.
 #' @examples
 #' \dontrun{
 #' if(interactive()){
@@ -182,5 +245,48 @@ agglomerate_metacoder <- function(obj, rank, valid_formats = c("raw_format", "ba
   return(mo_clone)
 }
 
-
+#' @title Taxonomic Prevalence Filter (Metacoder)
+#' @description This function filters observations at a specific rank by thier prevelance across samples.
+#' @param obj A Taxmap/metacoder object.
+#' @param rank The rank being analyzed for prevalence across samples.
+#' @param minimum_abundance The minimum abundance needed per observation per sample.  Default: 5
+#' @param rel_sample_percentage The percentage of samples per observation that meet the minimum abundance.  Default: 0.5
+#' @param valid_formats A vector of formats that are used for validation.  Default: c("basic_format")
+#' @param validated This parameter provides a way to override validation steps.  Use carefully.  Default: FALSE
+#' @return Returns a taxmap object that contains taxon_ids that have passed the above fiter.
+#' @pretty_print TRUE
+#' @details The taxa_prevalence_filter filters taxon_ids that do not appear more than a certian amount of times (minimum abundance) in a certain percentage of
+#' samples (rel_sample_percentage) at the specified agglomerated rank (rank). The \href{http://web.stanford.edu/class/bios221/MicrobiomeWorkflowII.html#filtering}{phyloseq workflow} calls for a minimum abundance of 5 across %50 of the samples.
+#' This method is considered supervised, because the filtering is done based on taxonomic annotation, which is assigned based on a reference database (SILVA or GreenGenes).
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @export
+#' @family Metacoder Filters
+#' @rdname taxa_prevalence_filter
+#' @seealso
+#'  \code{\link[MicrobiomeR]{validate_MicrobiomeR_format}}
+#'  \code{\link[metacoder]{calc_prop_samples}}
+#'  \code{\link[dplyr]{filter}}
+#'  \code{\link[taxa]{filter_taxa}}
+#' @importFrom metacoder calc_prop_samples
+#' @importFrom dplyr filter
+#' @importFrom taxa filter_taxa
+taxa_prevalence_filter <- function(obj, rank, minimum_abundance = 5, rel_sample_percentage = 0.5,
+                                   valid_formats = c("basic_format"), validated = FALSE) {
+  mo_clone <- obj$clone()
+  mo_clone <- validate_MicrobiomeR_format(obj = mo_clone, valid_formats = valid_formats,
+                                          force_format = TRUE, validated = validated, min_or_max = min)
+  # Calculate the ids that need to be removed based on taxonomic rank
+  ids_to_remove <- agglomerate_metacoder(obj = mo_clone, rank = rank,
+                                         valid_formats = c("basic_format"), validated = TRUE) %>% # Agglomeration
+    metacoder::calc_prop_samples("taxa_abundance", more_than = minimum_abundance) %>% # Calculate sample proportions per taxa with min abundance
+    dplyr::filter(n_samples < rel_sample_percentage) # Filter samples with less than the relative sample percentage
+  # Taxonomic Prevalence Filtering
+  mo_clone <- taxa::filter_taxa(mo_clone, !taxon_ids %in% ids_to_remove$taxon_id, reassign_obs = FALSE)
+  return(mo_clone)
+}
 
