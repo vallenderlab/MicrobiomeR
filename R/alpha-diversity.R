70399dc403fe2e15248f1c8683a8423d26a59360
sdhutchins <ms.s.hutchins@gmail.com>
2018-12-10 19:03:57 -0600

    Changed parameter to obj.

--- R/alpha-diversity.R
+++ R/alpha-diversity.R
@@ -2,16 +2,16 @@
 #'
 #' @description This function generates various alpha diversity measures include Shannon, Fisher, Coverage, Gini Simpson, and Inverse Simpson.
 #'
-#' @param phyloseq_object A preprocessed phyloseq object.
+#' @param phyloseq_object A phyloseq or metacoder object.
 #'
 #' @return Returns a list object of alpha diversity measurements.
 #' @importFrom microbiome diversities meta
 #' @importFrom utils combn
 #' @export
-get_alpha_diversity <- function(phyloseq_object) {
+get_alpha_diversity <- function(obj) {
   # Get all of the diversities.
-  divs <- microbiome::diversities(phyloseq_object, index = "all")
-  phyloseq_object.meta <- microbiome::meta(phyloseq_object)
+  divs <- microbiome::diversities(obj, index = "all")
+  phyloseq_object.meta <- microbiome::meta(obj)
   phyloseq_object.meta$Shannon <- divs$shannon
   phyloseq_object.meta$InverseSimpson <- divs$inverse_simpson
   phyloseq_object.meta$GiniSimpson <- divs$gini_simpson
