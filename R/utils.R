2f4209c93d8e9535561923e3f0fea76e0ce8e50c
grabear <robgilmore127@gmail.com>
2018-12-17 15:48:02 -0600

    Removed deprecated mkdir function from utils.R

--- R/utils.R
+++ R/utils.R
@@ -44,34 +44,6 @@ create_pub_table <- function(dataframe) {
   )
 }
 
-#' @title Make a Directory
-#'
-#' @description This is a custom function for creating a directory.
-#'
-#' @param dirname The name of the directory to create
-#' @param path The path to create the directory in.
-#'
-#' @return Returns the absolute path of the directory.
-#' @pretty_print TRUE
-#' @details This function will create a directory only if it doesn't exist and then return the path of that directory.
-#' @export
-#' @rdname mkdir
-mkdir <- function(dirname, path = NULL) {
-  # Create the path for the directory
-  if (is.null(path)) {
-    path <- getwd()
-  }
-  dirpath <- file.path(path, dirname)
-  # Create and return the directory path
-  if (dir.exists(dirpath)) {
-    return(dirpath)
-  } else {
-    dir.create(dirpath)
-    return(dirpath)
-  }
-}
-
-
 #' @title Object handler
 #' @description A function that handles the conversion of objects to metacoder (taxa::taxmap) objects.
 #' @param obj An object that contains the data being analyzed.  Can be one of the following:
