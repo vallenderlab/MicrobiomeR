e1ec508396f8496d403f49bfdec7c5d16706f434
grabear <robgilmore127@gmail.com>
2018-12-17 10:28:11 -0600

    Cleaned up the old port.  Removed old params, added explicit package::function calls, etc.

--- R/heat-tree.R
+++ R/heat-tree.R
@@ -1,27 +1,26 @@
 ## Initialize a list for storing heat tree plots
-get_heat_tree_plots <- function(obj, database = NULL, load_flag = NULL, filter_flag = NULL, save_flag = NULL, ambiguous_filter = NULL,
-                                kingdom_filter = NULL, rank_list = NULL) {
+get_heat_tree_plots <- function(obj, rank_list = NULL) {
   if (is.null(rank_list)) {
-    rank_list <- ranks
+    rank_list <- pkg.private$rank_list
   }
   htrees <- list()
   # Create a metacoder object from a phyloseq/metacoder/RData file
-  metacoder_object <- object_handler(obj)
+  metacoder_object <- MicrobiomeR::object_handler(obj)
   # Create a list of heat_tree plots for saving
   for (rank in rank_list) {
     rank_level <- rank_index[[rank]]
-    smb_data <- metacoder_object %>% filter_obs(data = c("statistical_data", "taxa_proportions", "taxa_abundance", "stats_tax_data"), n_supertaxa < rank_level, drop_taxa = TRUE)
+    smb_data <- metacoder_object %>% taxa::filter_obs(data = c("statistical_data", "taxa_proportions", "taxa_abundance", "stats_tax_data"), n_supertaxa < rank_level, drop_taxa = TRUE)
     title <- sprintf("Bacterial Abundance (%s Level)", rank)
     default_heat_tree_parameters <- get_default_parameters(smb_data = smb_data, title = title, func_names = c("heat_tree"))
     # Filter by Taxonomy Rank and then create a heat tree.
     htrees[[rank]] <- do.call(what = "heat_tree", args = c(default_heat_tree_parameters))
     # Made plot title centered
-    htrees[[rank]] <- htrees[[rank]] + theme(
-      plot.title = element_text(hjust = 0.5),
-      text = element_text(size = 24, family = "Arial")
-    ) + ggtitle(title)
-    htrees[[rank]]
+    htrees[[rank]] <- htrees[[rank]] +
+      ggplot2::theme(
+        plot.title = ggplot2::element_text(hjust = 0.5),
+        text = ggplot2::element_text(size = 24, family = "Arial")) +
+      ggplot2::ggtitle(title)
   }
-  htrees[["metacoder_object"]] <- smb_data
+  htrees[["metacoder_object"]] <- metacoder_object
   return(htrees)
 }
