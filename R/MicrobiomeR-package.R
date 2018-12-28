d1efc7db8fafb321a79a6a6a0e9b100ffc49600e
grabear <robgilmore127@gmail.com>
2018-12-28 14:03:23 -0600

    Updated with details section.

--- R/MicrobiomeR-package.R
+++ R/MicrobiomeR-package.R
@@ -3,6 +3,9 @@
 #' A microbiome workflow package using phyloseq for data import, metacoder for data analysis,
 #' and ggplot2 for data visualization.
 #'
+#' The goal of this package is to enhance the use of the \code{\link[metacoder:metacoder]{metacoder}} package by
+#' providing tools that mimic the \code{\link[phyloseq:phyloseq-package]{phyloseq}} package.  The only difference being that
+#' the main data object is a \code{\link[taxa:taxmap]{taxmap object}} instead of a \code{\link[phyloseq:phyloseq-class]{phyloseq object}}.
 #'
 #'
 #'
