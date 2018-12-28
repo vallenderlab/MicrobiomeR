3b4e65e6edb99d87c3100e0fc9c713d5e4b7ceec
sdhutchins <ms.s.hutchins@gmail.com>
2018-12-28 17:36:08 -0600

    Added inital permanova function.

--- R/permanova.R
+++ R/permanova.R
@@ -1,3 +1,41 @@
+#' @title PERMANOVA significance test for group-level differences
+#' @description Evaluate whether the group(s) has a significant effect on overall gut microbiota composition.
+#' @param obj Import an unprocessed phyloseq object.
+#' @param distance_method Use the desired statistical method. The default method is "bray".
+#' @return Returns a list object of the data.
+#' @export
+permanova <- function(obj, distance_method = "bray") {
+  # Validate data format
+  metacoder_object <- validate_MicrobiomeR_format(
+    obj = object_handler(obj),
+    valid_formats = c("analyzed_format")
+  )
+  # TODO: Convert this to metacoder strictly.
+  phyloseq <- list()
+  otu <- microbiome::abundances(phyloseq_object)
+  meta <- microbiome::meta(phyloseq_object)
+  if (distance_method == "wunifrac" | distance_method == "unifrac") {
+    stop("Method is not integrated yet.")
+  } else {
+    phyloseq[["permanova"]] <- vegan::adonis(t(otu) ~ TreatmentGroup, data = meta, permutations = 99, method = distance_method)
+  }
+  phyloseq[["phyloseq"]] <- phyloseq_object
+
+  # Checking the homogeneity condition
+  # Note the assumption of similar multivariate spread among the groups
+  # ie. analogous to variance homogeneity
+  # Here the groups have signif. different spreads and
+  # permanova result may be potentially explained by that.
+  dist <- vegan::vegdist(t(otu))
+  phyloseq[["anova"]] <- anova(vegan::betadisper(dist, meta$TreatmentGroup))
+
+  # Investigate the top factors
+  # Show coefficients for the top taxa separating the groups
+  phyloseq[["coef"]] <- coefficients(phyloseq$permanova)["TreatmentGroup1", ]
+  phyloseq[["top.coef"]] <- phyloseq$coef[rev(order(abs(phyloseq$coef)))[1:50]]
+  return(phyloseq)
+}
+
 #' @title Top Coefficients Barplot
 #' @description FUNCTION_DESCRIPTION
 #' @param top_coefficients PARAM_DESCRIPTION
