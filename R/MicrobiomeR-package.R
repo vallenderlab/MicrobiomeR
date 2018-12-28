995507a949896b7ce29d304870631ba8257da37f
grabear <robgilmore127@gmail.com>
2018-12-28 12:30:18 -0600

    Added initial description.

--- R/MicrobiomeR-package.R
+++ R/MicrobiomeR-package.R
@@ -1,3 +1,11 @@
+#' MicrobiomeR
+#'
+#' A microbiome workflow package using phyloseq for data import, metacoder for data analysis,
+#' and ggplot2 for data visualization.
+#'
+#'
+#'
+#'
 #' @author Rob Gilmore and Shaurita Hutchins
 #' @name MicrobiomeR
 #' @docType package
