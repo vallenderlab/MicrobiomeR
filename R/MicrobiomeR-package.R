d780a4cff81a48c96ec3d3541ae1354c9ce8c979
grabear <robgilmore127@gmail.com>
2018-12-28 15:13:13 -0600

    Added Other Documentation section to package docs.

--- R/MicrobiomeR-package.R
+++ R/MicrobiomeR-package.R
@@ -7,6 +7,18 @@
 #' providing tools that mimic the \code{\link[phyloseq:phyloseq-package]{phyloseq}} package.  The only difference being that
 #' the main data object is a \code{\link[taxa:taxmap]{taxmap object}} instead of a \code{\link[phyloseq:phyloseq-class]{phyloseq object}}.
 #'
+#' @section Other Documentation:
+#' Below are some links to helpful R package used, while developing this package.
+#'
+#' \describe{
+#'   \item{metacoder}{\url{https://grunwaldlab.github.io/metacoder_documentation/}}
+#'   \item{taxa}{\url{https://github.com/ropensci/taxa}}
+#'   \item{phyloseq}{\url{http://joey711.github.io/phyloseq/}}
+#'   \item{microbiome}{\url{http://microbiome.github.io/microbiome/}}
+#'   \item{MicrobiomeR}{\url{https://github.com/vallenderlab/MicrobiomeR}
+#' }}
+#' The [MicrobiomeR_Workflows] and [MicrobiomeR_Formats] help sections are also useful for understanding the
+#' package.
 #'
 #'
 #' @author Rob Gilmore and Shaurita Hutchins
