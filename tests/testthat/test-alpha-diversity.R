0dbbbd2a8c44b7907ffc057c614ea258620994b9
sdhutchins <ms.s.hutchins@gmail.com>
2019-01-10 18:23:52 -0600

    Updated alpha-diversity test.

--- tests/testthat/test-alpha-diversity.R
+++ tests/testthat/test-alpha-diversity.R
@@ -8,7 +8,7 @@ data <- as_MicrobiomeR_format(raw_silva, format = "analyzed_format")
 
 
 test_that("basic alpha diversity plot works", {
-  expect_true(!is.null(alpha_diversity_plot(obj = data, measure = "shannon", select_otu_table = "otu_proportions", save = FALSE)))
+  expect_true(!is.null(alpha_diversity_plot(obj = data, measure = "shannon", select_otu_table = "otu_proportions")))
 })
 
 # Remove file created by test
