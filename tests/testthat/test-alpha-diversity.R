1c3a7aee40c5507a014ba19cc0fae4fb64642b63
sdhutchins <ms.s.hutchins@gmail.com>
2019-01-16 16:39:13 -0600

    Extended alpha diversity testing for all measures.

--- tests/testthat/test-alpha-diversity.R
+++ tests/testthat/test-alpha-diversity.R
@@ -7,7 +7,17 @@ context("Testing of alpha diversity measures and plot")
 data <- as_MicrobiomeR_format(raw_silva, format = "analyzed_format")
 
 
-test_that("basic alpha diversity plot works", {
+test_that("alpha diversity measures all exist", {
+  expect_equal(length(get_alpha_diversity_measures(data)), 10)
+  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$Shannon))
+  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$InverseSimpson))
+  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$GiniSimpson))
+  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$Fisher))
+  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$Coverage))
+  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$group.pairs))
+})
+
+test_that("base alpha diversity plot works", {
   expect_true(!is.null(alpha_diversity_plot(obj = data, measure = "shannon", select_otu_table = "otu_proportions")))
 })
 
