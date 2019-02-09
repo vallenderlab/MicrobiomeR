db8c16bc6d0fbe2979d14b3582252de1bf5c2e24
sdhutchins <ms.s.hutchins@gmail.com>
2019-02-08 19:43:49 -0600

    Added ordination_plots tests.

--- tests/testthat/test-ordination.R
+++ tests/testthat/test-ordination.R
@@ -9,10 +9,18 @@ data <- as_MicrobiomeR_format(raw_silva_2, format = "analyzed_format")
 
 test_that("basic ordination plot works", {
   expect_true(!is.null(ordination_plot(obj = data)))
+  expect_true(!is.null(ordination_plot(obj = data, title = "Test Plot")))
 })
 
 test_that("basic ordination data return works", {
   expect_true(!is.null(ordination_plot(obj = data, only_data = TRUE)))
+  expect_true(!is.null(ordination_plot(obj = data, only_data = TRUE, title = "Test Plot")))
+})
+
+test_that("creating multiple ordination plots works", {
+  expect_true(!is.null(ordination_plots(obj = data)))
+  expect_true(!is.null(ordination_plots(obj = data, methods = NULL, distances = c("wunifrac", "unifrac", "bray"))))
+  expect_true(!is.null(ordination_plots(obj = data, methods = c("PCoA", "NMDS"), distances = NULL)))
 })
 
 # Remove file created by test
