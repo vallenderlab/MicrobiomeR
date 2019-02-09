4d60f3270433891a1856f559663b2d9f7839b97b
sdhutchins <ms.s.hutchins@gmail.com>
2019-02-08 19:44:20 -0600

    Added stacked_barplots test.

--- tests/testthat/test-barplot.R
+++ tests/testthat/test-barplot.R
@@ -8,8 +8,12 @@ data <- as_MicrobiomeR_format(raw_silva_2, format = "analyzed_format")
 test_palette <- get_color_palette(color_no = 12)
 
 
-test_that("basic barplot works", {
+test_that("basic stacked barplot works", {
   expect_true(!is.null(stacked_barplot(obj = data, palette_values = test_palette)))
+})
+
+test_that("creating multiple stacked barplots works", {
+  expect_true(!is.null(stacked_barplots(obj = data)))
 })
 
 # Remove file created by test
