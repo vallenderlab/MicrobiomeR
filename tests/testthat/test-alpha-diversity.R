721c619d39a7f7d0e6ac5ea731cc764e45fa454e
sdhutchins <ms.s.hutchins@gmail.com>
2019-02-08 19:41:06 -0600

    Updated alpha-diversity tests.

--- tests/testthat/test-alpha-diversity.R
+++ tests/testthat/test-alpha-diversity.R
@@ -8,17 +8,21 @@ data <- as_MicrobiomeR_format(raw_silva_2, format = "analyzed_format")
 
 
 test_that("alpha diversity measures all exist", {
-  expect_equal(length(get_alpha_diversity_measures(data)), 10)
-  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$Shannon))
-  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$InverseSimpson))
-  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$GiniSimpson))
-  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$Fisher))
-  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$Coverage))
-  expect_true(!is.null(get_alpha_diversity_measures(obj = data)$group.pairs))
+  expect_equal(length(alpha_diversity_measures(data)), 10)
+  expect_true(!is.null(alpha_diversity_measures(obj = data)$Shannon))
+  expect_true(!is.null(alpha_diversity_measures(obj = data)$InverseSimpson))
+  expect_true(!is.null(alpha_diversity_measures(obj = data)$GiniSimpson))
+  expect_true(!is.null(alpha_diversity_measures(obj = data)$Fisher))
+  expect_true(!is.null(alpha_diversity_measures(obj = data)$Coverage))
+  expect_true(!is.null(alpha_diversity_measures(obj = data)$group.pairs))
 })
 
 test_that("base alpha diversity plot works", {
   expect_true(!is.null(alpha_diversity_plot(obj = data, measure = "shannon", select_otu_table = "otu_proportions")))
+})
+
+test_that("creating multiple alpha diversity plots works", {
+  expect_true(!is.null(alpha_diversity_plots(obj = data)))
 })
 
 # Remove file created by test
