b414ed3ba57f856aea196317699745595f755bda
grabear <robgilmore127@gmail.com>
2020-07-08 12:48:27 -0500

    Added biomodality_coefficient function from modes.

--- R/utils.R
+++ R/utils.R
@@ -408,6 +408,68 @@ create_pub_table <- function(dataframe) {
 }
 
 
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param finite PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @export
+#' @rdname skewness
+skewness <- function(x, finite=TRUE) {
+  n=length(x)
+  S=(1/n)*sum((x-mean(x))^3)/(((1/n)*sum((x-mean(x))^2))^1.5)
+  if(finite==FALSE){
+    S=S
+  } else {
+    S=S*(sqrt(n*(n-1)))/(n-2)
+  }
+  return(S)
+}
+
+
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param finite PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @export
+#' @rdname kurtosis
+kurtosis <- function(x, finite) {
+  n=length(x)
+  K=(1/n)*sum((x-mean(x))^4)/(((1/n)*sum((x-mean(x))^2))^2) - 3
+  if(finite==FALSE) {
+    K=K
+  } else {
+    K=((n-1)*((n+1)*K - 3*(n-1))/((n-2)*(n-3))) +3
+  }
+  return(K)
+}
+
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param finite PARAM_DESCRIPTION, Default: TRUE
+#' @param ... PARAM_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @export
+#' @rdname bimodality_coefficient
+bimodality_coefficient <- function(x, finite=TRUE, ...) {
+  if(finite==TRUE) {
+    G=skewness(x,finite)
+    sample.excess.kurtosis=kurtosis(x,finite)
+    K=sample.excess.kurtosis
+    n=length(x)
+    B=((G^2)+1)/(K+ ((3*((n-1)^2))/((n-2)*(n-3))))
+  } else {
+    G=skewness(x,FALSE)
+    K=kurtosis(x,FALSE)
+    B=((G^2)+1)/(K)
+  }
+  return(B)
+}
+
+
+
 ####################### Private Package Variables #######################
 pkg.private <- new.env(parent = emptyenv())
 
